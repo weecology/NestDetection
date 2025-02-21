4233a05e82102553cc263c35c0f94964fe6b7378
Ernest <skmorgane@ufl.edu>
2025-02-21 13:56:12 -0500

    Clean up doc strings and variable names

--- field_nest_comparison.R
+++ field_nest_comparison.R
@@ -1,32 +1,11 @@
 library(dplyr)
 library(ggplot2)
 
-# Combine observer records with field nest records
-obs_data = read.csv("exp-field-nest-eval_072423.csv")
-field_data = read.csv("field_nest_sample_locations.csv")
-field_data = field_data |> filter(year == 2022)
-field_data = field_data |> filter(site != "CypressCity")
-
-obs_data$nest_id = paste(obs_data$year,"_",obs_data$colony,
-                         "_",obs_data$sample_id, sep="")
-obs_data = obs_data %>% filter(colony != "Joule")
-field_data$nest_id = paste(field_data$year,"_",field_data$site,
-                         "_",field_data$sample_id, sep="")
-
-field_bycolony = field_data |> count(site)
-obs_bycolony = obs_data |> count(colony)
-merge_obs = inner_join(obs_data, field_data)
-
-
-# Filter out the random bird records and uncertain human assessments
-field_nests = merge_obs %>% filter(real_nest == "yes") |> mutate(real_nest = "T") |>
-  filter(human_detect != "U")
-
-calc_humanrecall = function(data, detect_col, ref_col, detect_type){
+calc_recall = function(data, detect_col, ref_col, detect_type){
   
-  n_refnests = sum(data[[ref_col]] == "T") # reference column. Either field nests or human detects
-  n_Udetects = sum(data[[detect_col]] == "U") # uncertain code in test detection
-  test_detections = sum(data[[detect_col]] == "T") # test detections
+  n_refnests = sum(data[[ref_col]] == "T") # Either field nests or human detects
+  n_Udetects = sum(data[[detect_col]] == "U") # number of Uncertains
+  test_detections = sum(data[[detect_col]] == "T") # Nest detections
   test_recall = test_detections/(n_refnests - n_Udetects) # recall where U excluded 
   test_recall_wU = test_detections/(n_refnests) # recall with U counted as a fail
   results = data.frame(detect_type, n_refnests, n_Udetects, test_detections, test_recall, test_recall_wU)
@@ -34,16 +13,55 @@ calc_humanrecall = function(data, detect_col, ref_col, detect_type){
   return(results)
 }
 
-recall_results = calc_humanrecall(field_nests, "human_detect", "real_nest", "human-real")
-  
-recall_results = rbind(recall_results, calc_humanrecall(field_nests,
-                                                          "AI_nest", 
-                                                          "real_nest", 
-                                                          "computer-real"))
-recall_results = rbind(recall_results, calc_humanrecall(field_nests,
-                                                          "AI_nest", 
-                                                          "human_detect", 
-                                                          "computer-human"))
+# DATA PROCESSING
+## Load observer and algorithm observations and field nest records
+obs_data = read.csv("exp-field-nest-eval_072423.csv")
+field_data = read.csv("field_nest_sample_locations.csv")
+
+## Filter field data records 
+### field data includes 2021 data (our first attempt at flagging nests that revealed how difficult it was)
+### and a colony without ground control points
+field_data = field_data |> filter(year == 2022)
+field_data = field_data |> filter(site != "CypressCity")
+
+## Create unique nest_ids for each record 
+obs_data$nest_id = paste(obs_data$year,"_",obs_data$colony,
+                         "_",obs_data$sample_id, sep="")
+obs_data = obs_data %>% filter(colony != "Joule")
+field_data$nest_id = paste(field_data$year,"_",field_data$site,
+                         "_",field_data$sample_id, sep="")
+
+## Join field and observer files on unique nest id
+merge_obs = inner_join(obs_data, field_data)
+
+# Filter data to keep only real nests and remove uncertain human assessments
+## Also changed real_nest = "yes" to T for consistency.
+field_nests = merge_obs %>% filter(real_nest == "yes") |> 
+  mutate(real_nest = "T") |>
+  filter(human_detect != "U")
+
+# Generate raw sample sizes
+field_bycolony = field_data |> count(site)
+obs_bycolony = obs_data |> count(colony)
+
+# Calculate Recall
+## Human observations
+recall_results = calc_recall(field_nests, 
+                             "human_detect", 
+                             "real_nest", 
+                             "human-real")
+
+## Calculate and add algorithm results  
+recall_results = rbind(recall_results, calc_recall(field_nests,
+                                                   "AI_nest",
+                                                   "real_nest",
+                                                   "computer-real"))
+## Calculate and add human vs. algorithm results 
+recall_results = rbind(recall_results, calc_recall(field_nests,
+                                                   "AI_nest", 
+                                                   "human_detect", 
+                                                   "computer-human"))
+
 
 detections_colony = field_nests %>% group_by(colony) %>% 
   summarise(total=length(year), human_U = sum(human_detect == "U"), human=sum(human_detect=="T"),
@@ -62,17 +80,12 @@ detections_colony = detections_colony |> mutate(human_recall = human/(total - hu
 
 total_nests=sum(detections_colony$total)
 
-ggplot(detections_colony, aes(x=human_recall, y=computer_recall)) +
-  geom_point(aes(shape = colony), size = 5) + xlim(0.5,1) + ylim(0.5,1) +
-  theme(axis.title = element_text(size = 20), axis.text = element_text(size= 15)) +
-  geom_abline(intercept=0, slope = 1, color="blue") +
-  xlab("human") + ylab("computer") +
-  ggtitle("Recall")
-# recall
-#combined records for all ground monitored nests
-write.csv(field_nests, "field__nest_records.csv")
-
-# records of nests where human and AI disagreed
+# Find human and algorithm disagreements for post-hoc assessment
 disagreedTF = field_nests |> filter(human_detect =="T" & AI_nest == "FALSE")
 disagreedFT = field_nests |> filter(human_detect =="F" & AI_nest == "TRUE")
 uncertain = field_nests |> filter(human_detect =="U")
+
+# write combined records for all ground monitored nests
+write.csv(field_nests, "field__nest_records.csv")
+
+
